1a6eab9e9090e2126475ac63e1cf2e2860140de2
Lucas <lucaspaterno181@gmail.com>
2024-09-06 19:26:30 -0300

    Mudança tela, adição de imagnes

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
+const inico_btn = document.querySelector("#inicio"); 
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
@@ -8,4 +9,8 @@ sign_up_btn.addEventListener("click", () => {
 
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
+});
+
+inico_btn.addEventListener("click", () => {
+  window.location.href = 'telahome.html';
 });
