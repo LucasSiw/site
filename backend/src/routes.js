404cb7a369cbf6cda83b7abf918cd1b73bdcce05
Lucas <lucaspaterno181@gmail.com>
2024-09-10 18:41:46 -0300

    Retirando React

--- backend/src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-
-routes.post('/login', (req, res) => {
-    const {email, password} = req.body;
-    res.send('Login endpoint');
-});
-
-module.exports = routes;
