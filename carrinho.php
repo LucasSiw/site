874856780c42fb580d45d79f2289820b3fd9f635
Lucas <lucaspaterno181@gmail.com>
2024-10-04 18:59:31 -0300

    Melhorias de codigo

--- carrinho.php
+++ carrinho.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include_once('header.php'); 
 include_once(__DIR__ . '/php/conexao.php'); // Conexão com o banco de dados
 
 // Verifica se o usuário está logado
@@ -40,42 +41,190 @@ if (isset($_GET['remove'])) {
 }
 
 ?>
+<style>
+    
+.content {
+    display: flex;
+    margin-top: 100px; 
+}
 
-<!DOCTYPE html>
-<html lang="pt-br">
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Carrinho de Compras</title>
-    <link rel="stylesheet" href="/apetrecho/css/carrinho.css" />
-    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
-    <link rel="shortcut icon" href="/apetrecho/img/Apetrecho.ico" type="image/x-icon">
-</head>
+table thead tr {
+  border-bottom: 3px solid #eee;
+}
 
-<body>
-    <div class="wrapper">
-        <nav class="nav">
-            <div class="nav-logo">
-                <p>Apetrecho</p>
-            </div>
-            <div class="nav-menu" id="navMenu">
-                <ul>
-                    <li><a href="telahome.php" class="link <?= $current_page == 'telahome.php' ? 'active' : '' ?>">Inicial</a></li>
-                    <li><a href="carrinho.php" class="link <?= $current_page == 'carrinho.php' ? 'active' : '' ?>">Carrinho</a></li>
-                    <li><a href="#" class="link <?= $current_page == 'servicos.php' ? 'active' : '' ?>">Serviços</a></li>
-                    <li><a href="cadastrarproduto.php" class="link <?= $current_page == 'cadastrarproduto.php' ? 'active' : '' ?>">Cadastrar</a></li>
-                </ul>
-            </div>
-            <div class="nav-button">
-                <?php if (isset($_SESSION['usuario_nome'])): ?>
-                    <span class="welcome-message">Bem-vindo, <?= htmlspecialchars($_SESSION['usuario_nome']); ?>!</span>
-                <?php else: ?>
-                    <button class="btn white-btn" id="loginBtn" onclick="login()">Entrar</button>
-                <?php endif; ?>
-            </div>
-        </nav>
+table thead tr th {
+  text-align: left;
+  padding-bottom: 10px;
+  text-transform: uppercase;
+  color: #666;
+}
+
+table tbody tr {
+  border-bottom: 3px solid #eee;
+}
+
+table tbody tr:last-child {
+  border: 0;
+}
+
+table tbody tr td {
+  padding: 30px 0;
+}
+
+.produtos {
+    flex: 2;
+    padding-right: 20px;
+}
+
+.product {
+  display: flex;
+  align-items: center;
+}
+
+.product img {
+  border-radius: 6px;
+  width: 80px; 
+  height: auto;
+}
+
+.product .info {
+  margin-left: 20px;
+}
+
+.product .info .name {
+  font-size: 20px;
+  margin-bottom: 10px;
+}
+
+.product .info .category {
+  color: #666;
+}
+
+.qty {
+  background: #eee;
+  display: inline-flex;
+  justify-content: space-around;
+  align-items: center;
+  min-width: 60px;
+  border-radius: 20px;
+  overflow: hidden;
+  height: 30px;
+}
+
+.qty span {
+  margin: 0 10px;
+}
+
+.qty button {
+  background: transparent;
+  border: 0;
+  padding: 0 10px;
+  font-size: 20px;
+  height: 100%;
+}
+
+.qty button:hover {
+  background: #ddd;
+}
+
+.remove {
+  background: #eee;
+  border: 0;
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+}
+
+.remove:hover {
+  background: #ddd;
+}
+
+.resumo {
+    flex: 1; 
+    background: #fff;
+    border-radius: 6px;
+    padding: 15px;
+}
+
+.resumo .box {
+  background: #f9f9f9;
+  border-radius: 6px;
+  padding: 15px;
+}
+
+.resumo header {
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 20px;
+}
+
+.resumo .info {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+}
+
+.resumo footer {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+  font-weight: 600;
+}
+
+.resumo button {
+  background: #ffb742;
+  color: #fff;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  border-radius: 6px;
+  cursor: pointer;
+}
+
+.resumo button:hover {
+  background: #fb8c00;
+}
+
+.page-title {
+    text-align: center;
+    font-size: 28px;
+    margin: 100px 0 50px;
+    color: #fff;
+}
+
+.resumo .info {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 15px;
+}
+
+.resumo .info div {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 10px 5px; 
+}
+
+.resumo footer {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+    font-weight: 600;
+}
+
+.texto {
+  color: #fff;
+}
+</style>
 
         <div class="page-title">Seu Carrinho</div>
         <div class="content">
@@ -105,7 +254,7 @@ if (isset($_GET['remove'])) {
                             <?php endforeach; ?>
                         <?php else: ?>
                             <tr>
-                                <td colspan="5">Seu carrinho está vazio.</td>
+                                <td colspan="5" class="texto">Seu carrinho está vazio.</td>
                             </tr>
                         <?php endif; ?>
                     </tbody>
@@ -134,6 +283,4 @@ if (isset($_GET['remove'])) {
         </div>
     </div>
 
-</body>
-
-</html>
+<?php include_once('footer.php'); ?>
